6f8bb1da8264d45c15de34f897c6245a0fb79eb8
camila-romero-cyb <camilaromero085@gmail.com>
2022-11-02 22:17:28 -0300

    faltan los filtro

--- archivo.js
+++ archivo.js
@@ -9,35 +9,35 @@ modo.addEventListener("click", function () {
 })
 
 
+
+
+
+let sectionFilter = document.querySelector("#section-text");
+let sectionImage = document.querySelector("#sectionImage");
+let btnImg = document.querySelector("#btnImg");
+let btnText = document.querySelector("#btnText");
+
+btnText.addEventListener("click", function () {
+  sectionFilter.style.display = "block";
+  sectionImage.style.display = "none";
+})
+
+
+btnImg.addEventListener("click", function () {
+  sectionImage.style.display = "block";
+  sectionFilter.style.display = "none";
+  
+}) 
+
 /* seccion imagen*/
 
-
-const mostrar = (elemento) => {
-  elemento.style.display = 'block';
-}
-const ocultar = (elemento) => {
-  elemento.style.display = 'none';
-}
-const mostrarOcultar = (id) => {
-  let div = document.getElementById(id);
-  if( window.getComputedStyle(div).display !== 'none' ){
-    ocultar(div);
-    return false;
-  }
-
-  mostrar(div);
-}
-
-let contImg = document.querySelector("#visual-image");
+let contImg = document.querySelector("#visualImage");
 let imputImg = document.getElementById("imageName");
 imputImg.addEventListener("input", function (event) {
   contImg.style.backgroundImage =`url(${event.target.value})`
 
    console.log(event.target.value)
  })
-
-
-
 
 
 
@@ -68,8 +68,19 @@ inputBotton.addEventListener('input', (Event) => {
   
 })
 
-/* imput*/
+/*boton descarga*/
 
 
 
 
+
+/*let divImg = getElementById("primaryBox");
+let buttonDesc = getElementById("buttonDesc");
+
+const descMeme = () => {
+  domtoimage.toBlob('CAMBIAR').then(funcion(blob)); {
+    saveAs(blob, 'meme-jpg');
+  }
+  }
+
+buttonDesc.addEventListener("click", descMeme);*/
